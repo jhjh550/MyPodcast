90cede21433f73828c99e9d9881a2aec11488a61
jh <jhjh550@gmail.com>
2021-01-15 03:08:40 +0900

    add db create code

--- app/src/main/java/kr/co/jhjh550/mypodcast/db/PlayDataBase.kt
+++ app/src/main/java/kr/co/jhjh550/mypodcast/db/PlayDataBase.kt
@@ -1,9 +1,24 @@
 package kr.co.jhjh550.mypodcast.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [PlayData::class], version = 1)
 abstract class PlayDataBase: RoomDatabase() {
     abstract fun playDataDao(): PlayDataDao
+
+    companion object{
+        @Volatile private var instance: PlayDataBase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){
+            instance ?: createDatabase(context).also { instance = it }
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext,
+            PlayDataBase::class.java, "playdatabase.db").build()
+    }
 }
